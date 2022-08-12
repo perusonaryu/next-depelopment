946e174a4959e1be5e48fce5e97c7d29ab4231ac
perusonaryu <perusonaryu1127@gmail.com>
2022-08-13 00:51:50 +0900

    🐛 スマホメニュー表示時のスクロール不可対応

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { Burger } from '@mantine/core';
 import Link from 'next/link';
 import { Nav, SpNav } from 'src/components/Nav';
@@ -7,6 +7,14 @@ export const Header: FC = () => {
   const [opened, setOpened] = useState(false);
   const title = opened ? 'Close navigation' : 'Open navigation';
 
+  useEffect(() => {
+    if (opened) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [opened]);
+
   return (
     <>
       <header className="relative w-full">
@@ -14,10 +22,9 @@ export const Header: FC = () => {
           opened={opened}
           onClick={() => {
             setOpened((o) => !o)
-
-        }}
+          }}
           title={title}
-          className="absolute top-[25%] left-4 xs:hidden z-20 text-white"
+          className="absolute top-[25%] left-4 z-20 text-white xs:hidden"
           color={opened ? '#fff' : '#25262B'}
         />
         <div className="mx-4 flex h-[65px] max-w-screen-md items-center justify-center xs:m-auto xs:justify-between">
