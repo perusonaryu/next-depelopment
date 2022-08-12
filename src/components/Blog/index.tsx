10b3294cafa7728e9983bf3c4b20ee243a3eb455
perusonaryu <perusonaryu1127@gmail.com>
2022-08-12 14:34:49 +0900

    ♻️ 微修正

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -6,7 +6,6 @@ export const Blog = () => {
       <h2 className="mb-5 text-[26px] font-bold">Blog</h2>
       <div className="mb-6 w-full border-b-2 border-b-m_gray-2"></div>
       <BlogItem />
-      <div></div>
     </div>
   );
 };
--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -7,7 +7,7 @@ type Body = {
 
 export const Layout: FC<Body> = (props) => {
   return (
-    <div className="mt-11 mb-9  text-[#25262B] xs:my-0">
+    <div className="mt-11 mb-9 text-[#25262B] xs:my-0">
       <Header />
       <div>{props.children}</div>
     </div>
