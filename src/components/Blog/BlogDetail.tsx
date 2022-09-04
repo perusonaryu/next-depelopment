7eba126b789ed3957c16d1853cc3d47d3701e805
perusonaryu <perusonaryu1127@gmail.com>
2022-09-04 22:27:51 +0900

    ♻️ 日付フォーマット修正

--- src/components/Blog/BlogDetail.tsx
+++ src/components/Blog/BlogDetail.tsx
@@ -1,15 +1,19 @@
 import { blogItemType } from 'src/components/Blog/types';
+import moment from 'moment';
 
-
-export const BlogDetail:React.FC<blogItemType> = ({title,description,createdAt}) => {
+export const BlogDetail: React.FC<blogItemType> = ({
+  title,
+  description,
+  createdAt,
+}) => {
   return (
     <>
       <h2 className="mb-5 text-[26px] font-bold">{title}</h2>
       <div className="mb-6 w-full border-b-2 border-b-m_gray-2"></div>
-      <p className="mb-2 text-xs font-bold text-m_dark-2">{createdAt}</p>
-      <p className="text-base text-m_dark-6 line-clamp-2">
-        {description}
+      <p className="mb-2 text-xs font-bold text-m_dark-2">
+        {`${moment(createdAt).format('YYYY.MM.DD')} `}
       </p>
+      <p className="text-base text-m_dark-6 line-clamp-2">{description}</p>
     </>
   );
 };
